3b2bf275dd9ff624453b778b3f4d4ad155f191cc
Chandan Kumar <chandan.kr404@gmail.com>
2018-10-01 14:27:30 +0530

    Fetch gitlab pipelines from master branch
    Signed-off-by: Chandan Kumar <chandan.kr404@gmail.com>

--- src/gcp.js
+++ src/gcp.js
@@ -4,7 +4,7 @@ var gitlab_private_token = process.env.token;
 module.exports = {
     gcp_pipeline:function() {
         var gcp = {  
-            url: 'https://gitlab.openebs.ci/api/v4/projects/9/pipelines',
+            url: 'https://gitlab.openebs.ci/api/v4/projects/9/pipelines?ref=master',
             headers: {'PRIVATE-TOKEN': gitlab_private_token}
         };
         return new Promise(function(resolve, reject){
