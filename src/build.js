8beaff5330ec1fc1c9280e3c591e307caca353d2
Chandan Kumar <chandan.kr404@gmail.com>
2018-10-22 10:25:00 +0530

    Change project id of maya and jiva
    Signed-off-by: Chandan Kumar <chandan.kr404@gmail.com>

--- src/build.js
+++ src/build.js
@@ -22,7 +22,7 @@ module.exports = {
 
     maya_pipeline:function() {
         var maya = {
-            url: "https://gitlab.openebs.ci/api/v4/projects/25/pipelines?ref=master",
+            url: "https://gitlab.openebs.ci/api/v4/projects/31/pipelines?ref=master",
             headers: {'PRIVATE-TOKEN': gitlab_private_token}
         };
         return new Promise(function(resolve, reject){
@@ -41,7 +41,7 @@ module.exports = {
 
     jiva_pipeline:function() {
         var jiva = {
-            url: "https://gitlab.openebs.ci/api/v4/projects/22/pipelines?ref=master",
+            url: "https://gitlab.openebs.ci/api/v4/projects/28/pipelines?ref=master",
             headers: {'PRIVATE-TOKEN': gitlab_private_token}
         };
         return new Promise(function(resolve, reject){
